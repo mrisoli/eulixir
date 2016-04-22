f94e6fb3a728abe43fc405fcfbb8f0072448e524
mrisoli <celorisoli@gmail.com>
2016-04-22 18:40:18 -0300

    Adding problem 5

---
+++ lib/prob5.ex
@@ -0,0 +1,10 @@
+defmodule Prob5 do
+  def gcd(a,0), do: abs(a)
+  def gcd(a,b), do: gcd(b, rem(a,b))
+  def lcm(a,b), do: div(abs(a*b), gcd(a,b))
+
+  def run do
+    Enum.reduce(11..20, fn(x, acc) -> lcm(x,acc) end)
+    |> IO.puts
+  end
+end
