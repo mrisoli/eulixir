6901eea306a7cb9c1197030d9bbc7e0981864043
mrisoli <celorisoli@gmail.com>
2016-04-22 19:22:29 -0300

    Adding problem 9

---
+++ lib/prob9.ex
@@ -0,0 +1,14 @@
+defmodule Prob9 do
+  def triplet(a,b) do
+    c = 1000 - a - b
+    cond do
+      c * c == (a * a) + (b * b) -> a * b * c
+      a < 332 -> triplet( a + 1, b)
+      b < 497 -> triplet( 3, b + 1)
+    end
+  end
+
+  def run do
+    IO.puts(triplet(3,4))
+  end
+end
