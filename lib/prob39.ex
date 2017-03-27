85b83d8c4068f2946ec3d4aa63d45aa71cc004bf
mrisoli <celorisoli@gmail.com>
2017-03-27 19:51:43 +0200

    Adding problem 39

---
+++ lib/prob39.ex
@@ -0,0 +1,20 @@
+defmodule Prob39 do
+
+  def solutions(p, a) do
+    3..div(p, 3)
+    |> Enum.filter(fn(b) -> (a + b + :math.sqrt((a * a) + (b * b)) == p) end)
+    |> Enum.count
+  end
+
+  def num_solutions(p) do
+    4..div(p, 2)
+    |> Enum.map(&(solutions(p, &1)))
+    |> Enum.sum
+  end
+
+  def run do
+    3..1000
+    |> Enum.max_by(&num_solutions/1)
+    |> IO.puts
+  end
+end
